fa411e064d4123f4b3b05ae2d10047a71c64866f
hamzaboularbah <boularbah59@gmail.com>
2020-03-10 11:40:43 +0100

    added ssl config

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express"); // Express web server framework
-const request = require("request"); // "Request" library
-const cors = require("cors");
-const querystring = require("querystring");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+const https = require('https');
+const fs = require('fs');
 dotenv.config();
 
 const client_id = process.env.CLIENT_ID; // Your client id
@@ -13,7 +15,7 @@ const redirect_uri = process.env.REDIRECT_URI;
 const app = express();
 
 app
-  .use(express.static(__dirname + "/public"))
+  .use(express.static(__dirname + '/public'))
   .use(cors())
   .use(cookieParser());
 
@@ -23,9 +25,9 @@ app
  * @return {string} The generated string
  */
 const generateRandomString = length => {
-  let text = "";
+  let text = '';
   const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -33,28 +35,27 @@ const generateRandomString = length => {
   return text;
 };
 
-const stateKey = "spotify_auth_state";
+const stateKey = 'spotify_auth_state';
 
-
-
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = "streaming user-modify-playback-state app-remote-control user-read-playback-state user-read-currently-playing playlist-read-collaborative playlist-read-private user-read-private user-read-email";
+  const scope =
+    'streaming user-modify-playback-state app-remote-control user-read-playback-state user-read-currently-playing playlist-read-collaborative playlist-read-private user-read-private user-read-email';
   res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-    querystring.stringify({
-      response_type: "code",
-      client_id,
-      scope,
-      redirect_uri,
-      state
-    })
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id,
+        scope,
+        redirect_uri,
+        state,
+      })
   );
 });
 
-app.get("/callback", (req, res) => {
+app.get('/callback', (req, res) => {
   console.log(req.query);
   const code = req.query.code || null;
   const state = req.query.state || null;
@@ -62,26 +63,26 @@ app.get("/callback", (req, res) => {
 
   if (state === null || state !== storedState) {
     res.redirect(
-      "/#" +
-      querystring.stringify({
-        error: "state_mismatch"
-      })
+      '/#' +
+        querystring.stringify({
+          error: 'state_mismatch',
+        })
     );
   } else {
     res.clearCookie(stateKey);
     var authOptions = {
-      url: "https://accounts.spotify.com/api/token",
+      url: 'https://accounts.spotify.com/api/token',
       form: {
         code,
         redirect_uri,
-        grant_type: "authorization_code"
+        grant_type: 'authorization_code',
       },
       headers: {
         Authorization:
-          "Basic " +
-          new Buffer(client_id + ":" + client_secret).toString("base64")
+          'Basic ' +
+          new Buffer(client_id + ':' + client_secret).toString('base64'),
       },
-      json: true
+      json: true,
     };
 
     request.post(authOptions, (error, response, body) => {
@@ -90,9 +91,9 @@ app.get("/callback", (req, res) => {
           refresh_token = body.refresh_token;
 
         const options = {
-          url: "https://api.spotify.com/v1/me",
-          headers: { Authorization: "Bearer " + access_token },
-          json: true
+          url: 'https://api.spotify.com/v1/me',
+          headers: { Authorization: 'Bearer ' + access_token },
+          json: true,
         };
 
         // use the access token to access the Spotify Web API
@@ -101,50 +102,67 @@ app.get("/callback", (req, res) => {
         });
 
         res.redirect(
-          process.env.FRONTEND_URI + "auth/callback?" +
-          querystring.stringify({
-            access_token,
-            refresh_token
-          })
+          process.env.FRONTEND_URI +
+            'auth/callback?' +
+            querystring.stringify({
+              access_token,
+              refresh_token,
+            })
         );
       } else {
         res.redirect(
-          "/#" +
-          querystring.stringify({
-            error: "invalid_token"
-          })
+          '/#' +
+            querystring.stringify({
+              error: 'invalid_token',
+            })
         );
       }
     });
   }
 });
 
-app.get("/refresh_token", (req, res) => {
+app.get('/refresh_token', (req, res) => {
   const refresh_token = req.query.refresh_token;
   const authOptions = {
-    url: "https://accounts.spotify.com/api/token",
+    url: 'https://accounts.spotify.com/api/token',
     headers: {
       Authorization:
-        "Basic " +
-        new Buffer(client_id + ":" + client_secret).toString("base64")
+        'Basic ' +
+        new Buffer(client_id + ':' + client_secret).toString('base64'),
     },
     form: {
-      grant_type: "refresh_token",
-      refresh_token
+      grant_type: 'refresh_token',
+      refresh_token,
     },
-    json: true
+    json: true,
   };
 
   request.post(authOptions, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const access_token = body.access_token;
       res.send({
-        access_token: access_token
+        access_token: access_token,
       });
     }
   });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port 8888...");
-});
+if (process.env.ENV === 'production') {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync(process.env.CERTKEY),
+        cert: fs.readFileSync(process.env.CERT),
+        passphrase: process.env.PASSPHRASE,
+      },
+      app
+    )
+    .listen(8888);
+  console.log(
+    'Https : Spootify server app started on port : ' + process.env.PORT
+  );
+} else {
+  app.listen(process.env.PORT, () => {
+    console.log('Spootify server app started on port : ' + process.env.PORT);
+  });
+}
