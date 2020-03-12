4b10815285ecd0aff58759f8b2d08b90a7e82e46
hamzaboularbah <boularbah59@gmail.com>
2020-03-12 17:38:37 +0100

    added api port

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,5 +1,5 @@
 const Home = () => {
-  const loginURL = `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_API_DOMAIN} : ${process.env.REACT_APP_API_PORT}/login`;
+  const loginURL = `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_API_DOMAIN}:${process.env.REACT_APP_API_PORT}/login`;
   window.location = loginURL;
   return null;
 };
